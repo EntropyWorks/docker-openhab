97bb17aee344c942d47431368103b0b5f711c9d4
Angel Rodriguez-Ricardo <angelrr7702@gmail.com>
2015-10-08 10:45:12 -0400

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -5,10 +5,10 @@ set -e
 
 if [ -f /etc/configured ]; then
         echo 'already configured'
-        /opt/openhab/runtime/start.sh &
+        /opt/openhab/start.sh &
 else
         #code that need to run only one time ....
-        /opt/openhab/runtime/start.sh &
+        /opt/openhab/start.sh &
         #needed for fix problem with ubuntu and cron
         update-locale 
         date > /etc/configured
