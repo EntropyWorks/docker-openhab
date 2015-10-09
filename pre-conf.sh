50f423c33a44052f01567300562b1d9147202ba0
Angel Rodriguez-Ricardo <angelrr7702@gmail.com>
2015-10-08 23:56:49 -0400

    Update pre-conf.sh

--- pre-conf.sh
+++ pre-conf.sh
@@ -4,9 +4,9 @@ set -e
 mkdir /opt/openhab
 cd /opt/openhab
 
-curl "https://bintray.com/artifact/download/openhab/bin/openhab-2.0.0.alpha2-runtime.zip"
+wget -v https://bintray.com/artifact/download/openhab/bin/openhab-2.0.0.alpha2-runtime.zip
 unzip openhab-2.0.0.alpha2-runtime.zip
 rm README.TXT
-curl "https://bintray.com/artifact/download/openhab/bin/openhab-2.0.0.alpha2-addons.zip"
+wget -v https://bintray.com/artifact/download/openhab/bin/openhab-2.0.0.alpha2-addons.zip
 unzip openhab-2.0.0.alpha2-addons.zip
 rm openhab-2.0.0.alpha2-runtime.zip openhab-2.0.0.alpha2-addons.zip
