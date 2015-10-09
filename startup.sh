5cd82568013ef931df40c1fe583f42fd4f2cdf6f
Angel Rodriguez-Ricardo <angelrr7702@gmail.com>
2015-10-09 10:26:29 -0400

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -6,9 +6,11 @@ set -e
 if [ -f /etc/configured ]; then
         echo 'already configured'
         /opt/openhab/start.sh &
+        /etc/init.d/mosquitto start &
 else
         #code that need to run only one time ....
         /opt/openhab/start.sh &
+        /etc/init.d/mosquitto start &
         #needed for fix problem with ubuntu and cron
         update-locale 
         date > /etc/configured
