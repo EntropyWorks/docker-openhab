e7c618fcaf6fb768ba2737875e1768ae6f602397
Angel Rodriguez-Ricardo <angelrr7702@gmail.com>
2015-10-10 10:47:51 -0400

    Update pre-conf.sh

--- pre-conf.sh
+++ pre-conf.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 set -e
 
-/sbin/setuser mysql /usr/bin/mysqld_safe &
-sleep 10s
-
 mkdir /opt/openhab
 cd /opt/openhab
 curl -o /opt/openhab/openhab.zip -SL "https://bintray.com/artifact/download/openhab/bin/openhab-2.0.0.alpha2-runtime.zip"
@@ -13,13 +10,5 @@ curl -o /opt/openhab/openhabaddons.zip -SL "https://bintray.com/artifact/downloa
 unzip openhabaddons.zip
 rm openhab.zip openhabaddons.zip
 
-mysqladmin -u root -h 127.0.0.1 password mysqlpsswd
-mysqladmin -u root -h 127.0.0.1 -pmysqlpsswd reload
-mysqladmin -u root -h 127.0.0.1 -pmysqlpsswd create OpenHAB
 
-echo "grant select,insert,update,delete on OpenHAB.* to 'openhab'@127.0.0.1 identified by 'openhabpasswd'; flush privileges; " | mysql -u root -pmysqlpsswd
- 
-killall mysqld
-sleep 10s
- 
 mv -f /mysql.cfg /opt/openhab/conf/services/mysql.cfg
