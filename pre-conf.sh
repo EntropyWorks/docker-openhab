2c37bca09316338d3800f2144012e10b6e6eee8a
Angel Rodriguez-Ricardo <angelrr7702@gmail.com>
2015-10-10 11:46:10 -0400

    Update pre-conf.sh

--- pre-conf.sh
+++ pre-conf.sh
@@ -3,10 +3,10 @@ set -e
 
 mkdir /opt/openhab
 cd /opt/openhab
-curl -o /opt/openhab/openhab.zip -SL "https://bintray.com/artifact/download/openhab/bin/openhab-2.0.0.alpha2-runtime.zip"
+curl -o /opt/openhab/openhab.zip -SL "https://bintray.com/artifact/download/openhab/bin/distribution-1.7.1-runtime.zip"
 unzip openhab.zip
 rm README.TXT
-curl -o /opt/openhab/openhabaddons.zip -SL "https://bintray.com/artifact/download/openhab/bin/openhab-2.0.0.alpha2-addons.zip"
+curl -o /opt/openhab/openhabaddons.zip -SL "https://bintray.com/artifact/download/openhab/bin/distribution-1.7.1-addons.zip"
 unzip openhabaddons.zip
 rm openhab.zip openhabaddons.zip
 
