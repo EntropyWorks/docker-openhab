2eed9296090d4a9f4465cf41df7eeaa409e337ad
Angel Rodriguez-Ricardo <angelrr7702@gmail.com>
2015-10-11 09:37:34 -0400

    Update pre-conf.sh

--- pre-conf.sh
+++ pre-conf.sh
@@ -8,6 +8,8 @@ unzip openhab.zip
 rm README.TXT
 curl -o /opt/openhab/openhabaddons.zip -SL "https://bintray.com/artifact/download/openhab/bin/openhab-2.0.0.alpha2-addons.zip"
 unzip openhabaddons.zip
+curl -o /opt/openhab/openhabdemo.zip -SL "https://bintray.com/artifact/download/openhab/bin/openhab-2.0.0.alpha2-demo.zip"
+unzip openhabdemo.zip
 rm openhab.zip openhabaddons.zip
 
 
