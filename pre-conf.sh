cc3ada93b8a4d20574717f1a70b4ffdd95071f0d
Angel Rodriguez-Ricardo <angelrr7702@gmail.com>
2015-10-11 10:19:00 -0400

    Update pre-conf.sh

--- pre-conf.sh
+++ pre-conf.sh
@@ -5,12 +5,10 @@ mkdir /opt/openhab
 cd /opt/openhab
 curl -o /opt/openhab/openhab.zip -SL "https://bintray.com/artifact/download/openhab/bin/openhab-2.0.0.alpha2-runtime.zip"
 unzip openhab.zip
-rm README.TXT
 curl -o /opt/openhab/openhabaddons.zip -SL "https://bintray.com/artifact/download/openhab/bin/openhab-2.0.0.alpha2-addons.zip"
-unzip openhabaddons.zip
+unzip -o openhabaddons.zip
 curl -o /opt/openhab/openhabdemo.zip -SL "https://bintray.com/artifact/download/openhab/bin/openhab-2.0.0.alpha2-demo.zip"
-unzip openhabdemo.zip
+unzip -o openhabdemo.zip
 rm openhab.zip openhabaddons.zip openhabdemo.zip
-
 
 mv -f /mysql.cfg /opt/openhab/conf/services/mysql.cfg
